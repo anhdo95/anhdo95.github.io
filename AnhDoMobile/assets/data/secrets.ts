c1e43534a7e590ad672b72d9d4771f1eba3f2f16
Anh Do <dinhanhdo95@gmail.com>
2018-04-24 15:06:48 +0700

    Add files of AnhDoMobile project

---
+++ AnhDoMobile/assets/data/secrets.ts
@@ -0,0 +1,7 @@
+const secrets = {
+  facebookAppId: '1799200893717895',
+  googleClientId: '424004712022-u4sab3e55b5afh86j6qhpf84l1b0s0uc.apps.googleusercontent.com',
+  googleApiKey: 'AIzaSyCf9Sv96lHzIPPlWJ0GJuull-2Pst2aQzI'
+};
+
+export default secrets;
