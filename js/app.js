0e5904b4362ec6fd19bb32a196243c133227264d
anhdo <anh.dinh@savvycomsoftware.com>
2020-04-02 22:13:54 +0700

    Show an app installation for my cv

--- js/app.js
+++ js/app.js
@@ -4,3 +4,30 @@ if (navigator.serviceWorker) {
       console.log('[Service Worker] Registered')
     })
 }
+
+let deferredPrompt
+
+window.addEventListener('beforeinstallprompt', function(event) {
+  // Prevent the mini-infobar from appearing on mobile
+  event.preventDefault()
+  // Stash the event so it can be triggered later.
+  deferredPrompt = event
+})
+
+const sidebarButton = document.querySelector('.sidebar__button')
+
+sidebarButton.addEventListener('click', function(event) {
+  if (deferredPrompt) {
+    deferredPrompt.prompt()
+  
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+    })
+  
+    deferredPrompt = null
+  }
+})
